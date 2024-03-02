79cfc8c9efc781fe678aeb088eac5bfe32aa338c
Andrew Maclean <andrew.n.maclean@gmail.com>
2024-03-02 10:37:54 -0800

    Made a temp fix for a frontend issue. This should be reversed later. When creating a new conversation, the last_message was '', and now it's ' '. This avoids a check on the front end that calls a function that doesn't exist. To fix the front end, though it would need the whole app to be re-deployed and this is easier.

--- DB/Statements/Messages.js
+++ DB/Statements/Messages.js
@@ -3,13 +3,23 @@ const createNewMessageStatement =
 const addLastMessageStatement =
   'UPDATE conversations SET last_message = ? WHERE conversation_id = ?'
 const createNewConversationStatement =
-  "INSERT INTO conversations (last_message) VALUES ('')"
+  "INSERT INTO conversations (last_message) VALUES (' ')"
 const createNewInteractionsStatement =
   'INSERT INTO conversation_interactions (user_id, conversation_id, unread) VALUES ?'
 const findConversationStatement =
   'SELECT ci.conversation_id, c.last_message, c.conversation_name FROM conversation_interactions AS ci INNER JOIN conversations AS c ON c.id = ci.conversation_id WHERE conversation_id IN (SELECT conversation_id FROM conversation_interactions WHERE user_id IN ?) AND conversation_id NOT IN (SELECT conversation_id FROM conversation_interactions WHERE user_id NOT IN ?) GROUP BY conversation_id HAVING COUNT(ci.user_id) = ?'
-const getUserConversationsStatement =
-  "SELECT CONCAT(u.first_name, ' ', u.last_name) AS user_display_name, c.last_message, u.id AS user_id, u.profile_picture_url, ci.conversation_id, ci.unread FROM conversation_interactions AS ci INNER JOIN users AS u ON u.id = ci.user_id INNER JOIN conversations AS c ON c.id = ci.conversation_id WHERE conversation_id IN ( SELECT conversation_id FROM conversation_interactions WHERE user_id = ? )"
+const getUserConversationsStatement = `SELECT
+  CONCAT(u.first_name, ' ', u.last_name) AS user_display_name,
+  c.last_message,
+  u.id AS user_id,
+  u.profile_picture_url,
+  ci.conversation_id,
+  ci.unread
+  FROM conversation_interactions AS ci
+  INNER JOIN users AS u ON u.id = ci.user_id
+  INNER JOIN conversations AS c ON c.id = ci.conversation_id
+  WHERE conversation_id IN ( SELECT conversation_id FROM conversation_interactions WHERE user_id = ? )
+  ORDER BY ci.unread DESC`
 const setUnreadStatement =
   'UPDATE conversation_interactions SET unread = 1 WHERE conversation_id = ? AND user_id != ?'
 const setLastMessageStatement =
