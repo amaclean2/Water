06a1187f894990304c74fb90829703f0361c4603
Andrew Maclean <andrew.n.maclean@gmail.com>
2024-07-11 12:33:19 -0700

    force season should be an array

--- DB/DatabaseAdventures/index.js
+++ DB/DatabaseAdventures/index.js
@@ -205,9 +205,9 @@ class AdventureDataLayer extends DataLayer {
         selectedAdventure.date_created
       ).getTime()
 
-      if (selectedAdventure.season) {
-        selectedAdventure.season = JSON.parse(selectedAdventure.season)
-      }
+      selectedAdventure.season = selectedAdventure.season
+        ? JSON.parse(selectedAdventure.season)
+        : []
 
       // convert the stringified path back to an object
       // pathAdventures are any adventures that would have a path/elevations property
